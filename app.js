c998f826c1d8e73c3fda658c75697dab9897bf38
sophiafabri <sophiafabri07@gmail.com>
2024-11-26 15:55:36 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userController = require('./controllers/controllers');
+const app = express();
+
+// Configura o EJS como mecanismo de visualização
+app.set('view engine', 'ejs');
+
+// Middleware para parsing do body
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Rotas
+app.get('/', userController.getAllUsers);
+// app.get('/add', (req, res) => res.render('add'));
+// app.post('/add', userController.addUser);
+app.get('/edit/:id', userController.getUserById);
+app.post('/edit/:id', userController.updateUser);
+// app.get('/dell/:id', userController.getdeleteByUser);
+// app.post('/dell/:id', userController.deleteUser);
+
+// Iniciar o servidor
+app.listen(2000, () => {
+    console.log('Servidor rodando na porta 2000');
+});
